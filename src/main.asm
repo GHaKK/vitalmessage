6ce5e33b70c12557fc157609b6d4798839494500
Tommy Larsen <tommy.larsen@ghakk.se>
2024-07-07 21:32:15 +0200

    Header and some changes
    Added header in the code, commented some functions
    Removed CLS out into a label instead of Macro

--- src/main.asm
+++ src/main.asm
@@ -1,42 +1,57 @@
-section .data
-    cls db 27, "[2J", 27, "[H", 0       ; Escape characters to clear the screen
-                                        ; and move the pointer to the top right
-    headline db "          Vital Message      ", 10, 10 ; Headline
-    headline_length equ $-headline
-    difficulty db "   How difficult (4-10)? $ ", 0      ; Ask about difficulty
-    difficulty_buffer db 2
+;   Executable name                 : main
+;   Version                         : 1.0
+;   Created date                    : 2024-07-07
+;   Last Update                     : 2024-07-07
+;   Author                          : Tommy Larsen
+;   License                         : MIT
+;   GitHub-Link                     : https://github.com/GHaKK/vitalmessage.git
+;   Description                     : Game of chance where you select a number
+;                                     and a random number is generated
+;   
+;
+;   Makefile created for making, tree looks like this
+;       ./src (All source-code)
+;       ./build (Where binaries gets built)
+;       ./obj (Here the .o files ends up before it gets linked)
+;
+
+section .data                                               ; Section containing initialized data
+    cls db 27, "[2J", 27, "[H", 0                           ; Escape characters to clear the screen
+                                                            
+    headline db "          Vital Message      ", 10, 10
+    headline_length equ $-headline                          
+    difficulty db "   How difficult (4-10)? $ ", 0
     difficulty_length equ $-difficulty
 
 
-section .bss
+section .bss                                                ; Section containing uninitialized data
     input_difficulty resb 4
 
 
-section .text
+section .text                                               ; Here the code starts
     global _start
 
-
-%macro clear_screen_and_move 0
-    mov rax, 1                      ; sys_write
-    mov rdi, 1                  ; file descriptor (stdout)
-    lea rsi, [cls]    ; pointer to the escape sequence
-    mov rdx, 8          ; length of the sequence
-    syscall
-%endmacro
-
 %macro print_msg 2
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, %1
-    mov rdx, %2
-    syscall
+    mov rax, 1                          ; Set syscall "Write"
+    mov rdi, 1                          ; Set write to STDOUT
+    mov rsi, %1                         ; Message to print
+    mov rdx, %2                         ; Length of message (bytes)
+    syscall                             ; Call Kernel
 %endmacro
 
-%macro exit 0
-    mov rax, 60
-    mov rdi, 0
-    syscall
+%macro exit 0                           ; Exit Program
+    mov rax, 60                         ; Set sysExit
+    mov rdi, 0                          ; Set ExitCode = 0
+    syscall                             ; Call kernel
 %endmacro
+
+clear_screen_and_move:
+    mov rax, 1                          ; sys_write
+    mov rdi, 1                          ; file descriptor (stdout)
+    lea rsi, [cls]                      ; pointer to the escape sequence
+    mov rdx, 8                          ; length of the sequence
+    syscall
+    ret
 
 _dif_input:
     mov rax, 0
@@ -47,7 +62,7 @@ _dif_input:
     ret
 
 _start:
-    clear_screen_and_move
+    call clear_screen_and_move
     print_msg headline, headline_length
     print_msg difficulty, difficulty_length
     call _dif_input
